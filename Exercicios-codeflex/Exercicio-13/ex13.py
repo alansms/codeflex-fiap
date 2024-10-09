a9849dee3c9ff195d03f84615539d69f601d3cff
André Rovai Junior <82545873+dede0702@users.noreply.github.com>
2024-10-09 20:40:02 -0300

    ex13.py

--- Exercicios-codeflex/Exercicio-13/ex13.py
+++ Exercicios-codeflex/Exercicio-13/ex13.py
@@ -1,15 +1,45 @@
-# Solicitar ao usuário sua idade
-idade = int(input("Digite sua idade: "))
+def solicitar_idade() -> int:
+    """
+    Solicita a idade do usuário.
 
-# Classificar a pessoa na faixa etária correspondente
-if idade < 12:
-    classificacao = "Criança"
-elif 12 <= idade <= 17:
-    classificacao = "Adolescente"
-elif 18 <= idade <= 64:
-    classificacao = "Adulto"
-else:
-    classificacao = "Idoso"
+    Retorna:
+    int: Idade fornecida pelo usuário.
+    """
+    while True:
+        try:
+            idade = int(input("Digite sua idade: "))
+            if idade < 0:
+                print("A idade não pode ser negativa. Tente novamente.")
+            else:
+                return idade
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro.")
 
-# Exibir a classificação etária
-print(f"Você é classificado como: {classificacao}")
+def classificar_idade(idade: int) -> str:
+    """
+    Classifica a pessoa em uma faixa etária baseada na idade fornecida.
+
+    Parâmetros:
+    idade (int): A idade da pessoa.
+
+    Retorna:
+    str: A classificação etária ("Criança", "Adolescente", "Adulto", "Idoso").
+    """
+    if idade < 12:
+        return "Criança"
+    elif 12 <= idade <= 17:
+        return "Adolescente"
+    elif 18 <= idade <= 64:
+        return "Adulto"
+    else:
+        return "Idoso"
+
+if __name__ == "__main__":
+    # Solicitar a idade do usuário
+    idade = solicitar_idade()
+    
+    # Classificar a faixa etária
+    classificacao = classificar_idade(idade)
+    
+    # Exibir a classificação etária
+    print(f"Você é classificado como: {classificacao}")
